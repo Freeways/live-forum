855f3657de35a0b7ad83cef7b9c11f495fb9dc5e
bechir <bechir@accessnow.org>
2016-01-20 14:08:02 +0100

    Dynamic response body

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-   res.send('Hello World');
+    var percent = Math.round(Math.random() * 100);
+    res.send('Today I am ' + percent + '% willing to chat !');
 });
 
 var server = app.listen(8081, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log("Live forum listening at http://%s:%s", host, port)
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Live forum listening at http://%s:%s", host, port)
 });
