e77588b25a07cbd389d137677436a01bd7b95dde
bechir <bechir@accessnow.org>
2016-01-20 14:03:15 +0100

    Hello world

---
+++ README.md
@@ -0,0 +1,5 @@
+# Live Forum
+This project is done in the intention to initiate people to nodejs.
+
+The project is a small forum application accessible via OAuth and containing a chat feature.
+
--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 var express = require('express');
 var app = express();
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+});
+
+var server = app.listen(8081, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("Live forum listening at http://%s:%s", host, port)
+});
